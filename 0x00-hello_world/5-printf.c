120c63e2f47967f2daced8071311e9ee5d681a23
AgudaSegun <samuelsegunaguda@gmail.com>
2022-10-13 07:54:13 -0700

    The printing

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Printing text
+*
+* Return: Always 0 then its a  (Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
