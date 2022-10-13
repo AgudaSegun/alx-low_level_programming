67c9fd224b19d830fd197203cd1270f90ac40bdc
AgudaSegun <samuelsegunaguda@gmail.com>
2022-10-13 05:37:26 -0700

    Writing C programm

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *Main - Printing in C Prohramming Language
+ *
+ *Return - When 0 it is a success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
